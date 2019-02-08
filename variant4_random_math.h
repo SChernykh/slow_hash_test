5fe01a4f4eed939e599c71c9820e9b1d20450301
SChernykh <sergey.v.chernykh@gmail.com>
2019-02-08 23:18:24 +0100

    Updated to the latest code

--- variant4_random_math.h
+++ variant4_random_math.h
@@ -37,6 +37,7 @@ enum V4_InstructionList
 	V4_INSTRUCTION_COUNT = RET,
 };
 
+// V4_InstructionDefinition is used to generate code from random data
 // Every random sequence of bytes is a valid code
 //
 // There are 8 registers in total:
@@ -46,9 +47,9 @@ enum V4_InstructionList
 // This is why dst_index is 2 bits
 enum V4_InstructionDefinition
 {
-    V4_OPCODE_BITS = 3,
-    V4_DST_INDEX_BITS = 2,
-    V4_SRC_INDEX_BITS = 3,
+	V4_OPCODE_BITS = 3,
+	V4_DST_INDEX_BITS = 2,
+	V4_SRC_INDEX_BITS = 3,
 };
 
 struct V4_Instruction
@@ -169,11 +170,11 @@ static FORCEINLINE void v4_random_math(const struct V4_Instruction* code, v4_reg
 }
 
 // If we don't have enough data available, generate more
-static FORCEINLINE void check_data(size_t* data_index, const size_t bytes_needed, char* data, const size_t data_size)
+static FORCEINLINE void check_data(size_t* data_index, const size_t bytes_needed, int8_t* data, const size_t data_size)
 {
 	if (*data_index + bytes_needed > data_size)
 	{
-		hash_extra_blake(data, data_size, data);
+		hash_extra_blake(data, data_size, (char*) data);
 		*data_index = 0;
 	}
 }
@@ -196,10 +197,14 @@ static inline int v4_random_math_init(struct V4_Instruction* code, const uint64_
 	// Available ALUs for each instruction
 	const int op_ALUs[V4_INSTRUCTION_COUNT] = { ALU_COUNT_MUL, ALU_COUNT, ALU_COUNT, ALU_COUNT, ALU_COUNT, ALU_COUNT };
 
-	char data[32];
+	int8_t data[32];
 	memset(data, 0, sizeof(data));
-	*((uint64_t*)data) = height;
+	uint64_t tmp = SWAP64LE(height);
+	memcpy(data, &tmp, sizeof(uint64_t));
 
+	// Set data_index past the last byte in data
+	// to trigger full data update with blake hash
+	// before we start using it
 	size_t data_index = sizeof(data);
 
 	int code_size;
@@ -212,7 +217,7 @@ static inline int v4_random_math_init(struct V4_Instruction* code, const uint64_
 		// byte 1: instruction opcode
 		// byte 2: current value of the source register
 		//
-		// Registers R4-R7 are constant and are threatened as having the same value because when we do
+		// Registers R4-R7 are constant and are treated as having the same value because when we do
 		// the same operation twice with two constant source registers, it can be optimized into a single operation
 		int inst_data[8] = { 0, 1, 2, 3, -1, -1, -1, -1 };
 
@@ -358,7 +363,9 @@ static inline int v4_random_math_init(struct V4_Instruction* code, const uint64_
 
 					// ADD instruction requires 4 more random bytes for 32-bit constant "C" in "a = a + b + C"
 					check_data(&data_index, sizeof(uint32_t), data, sizeof(data));
-					code[code_size].C = *((uint32_t*)&data[data_index]);
+					uint32_t t;
+					memcpy(&t, data + data_index, sizeof(uint32_t));
+					code[code_size].C = SWAP32LE(t);
 					data_index += sizeof(uint32_t);
 				}
 
